a401df140104b9d70306e12c7fde471148fa164e
ilya sheprut <optozorax@gmail.com>
2021-02-07 18:06:42 +0700

    add error with info about this module

--- include.h
+++ include.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#error "Для начала прочитай https://github.com/klavarog/arbitrary_keycode и измени код QMK как там сказано, затем можно эту ошибку закомментировать."
+
 void press_arbitrary_keycode(uint16_t keycode, bool down) {
 	keyrecord_t record = {
 	  .event = {
